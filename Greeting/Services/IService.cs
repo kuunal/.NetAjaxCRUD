204471ba3ad0b8aa3c5829a6ab843d36fd59ce51
kuunal <kunaldeshmukh2503@gmail.com>
2020-11-25 02:13:34 +0530

    [Kunal] ADD: Abstraction for employee serive

---
+++ Greeting/Services/IService.cs
@@ -0,0 +1,14 @@
+﻿
+using Greeting.Models;
+using System.Collections.Generic;
+
+namespace Greeting.Services
+{
+    public interface IService
+    {
+        ServiceResponse<List<Employee>> GetEmployees();
+        ServiceResponse<Employee> GetEmployee(int id);
+        ServiceResponse<Employee> AddEmployee(Employee employee);
+
+    }
+}
