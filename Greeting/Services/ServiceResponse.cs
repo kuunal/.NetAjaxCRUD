d391d87ab4e995f5ac0e1c0a4bdcdf1c918f1692
kuunal <kunaldeshmukh2503@gmail.com>
2020-11-25 02:14:31 +0530

    [Kunal] ADD: Custom response class

---
+++ Greeting/Services/ServiceResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Greeting.Services
+{
+    public class ServiceResponse<T>
+    {
+        public T Data { get; set; }
+
+        public bool Success { get; set; } = true;
+        public string message { get; set; } = null;
+    }
+}
