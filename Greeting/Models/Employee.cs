5984894dd4632234855a8a05ddae606e34e2cbdd
kuunal <kunaldeshmukh2503@gmail.com>
2020-11-25 01:21:21 +0530

    [Kunal] ADD: Models for employee

---
+++ Greeting/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Greeting.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; } = 1;
+        public string Email { get; set; } = "f0rest@sk.com";
+        public string Name { get; set; } = "f0rest";
+        public string Password { get; set; } = "f0rest@123";
+
+    }
+}
