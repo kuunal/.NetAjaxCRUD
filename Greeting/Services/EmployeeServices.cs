3cf2a38fef2ae9376ced21c5938f88bc5f20c778
kuunal <kunaldeshmukh2503@gmail.com>
2020-11-25 18:02:19 +0530

    [Kunal] ADD: Automapper for conversion dao into dto

--- Greeting/Services/EmployeeServices.cs
+++ Greeting/Services/EmployeeServices.cs
@@ -1,4 +1,6 @@
-﻿using Greeting.Models;
+﻿using AutoMapper;
+using Greeting.DTOs.EmployeeDTO;
+using Greeting.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,30 +11,36 @@ namespace Greeting.Services
     public class EmployeeServices : IService
     {
         List<Employee> empList = new List<Employee>() {
-            new Employee(),
-            new Employee{ Id=2, Name="GEt_Right", Email="Get_Right@sk.com"},
-            new Employee{ Id=3, Name="fallEn", Email="fallen@sk.com"},
-            new Employee{ Id=4, Name="Spawn", Email="Spawn@sk.com"},
+            new Employee{ Id=1, Name="f0rest", Email="f0rest@sk.com", address="NY", PhoneNumber=1234567890},
+            new Employee{ Id=2, Name="GEt_Right", Email="Get_Right@sk.com", address="NY", PhoneNumber=1234567890},
+            new Employee{ Id=3, Name="fallEn", Email="fallen@sk.com", address="NY", PhoneNumber=1234567890},
+            new Employee{ Id=4, Name="Spawn", Email="Spawn@sk.com", address="NY", PhoneNumber=1234567890},
         };
-        public ServiceResponse<Employee> AddEmployee(Employee employee)
+
+        IMapper _mapper;
+        public EmployeeServices(IMapper mapper)
         {
-            ServiceResponse<Employee> serviceResponse = new ServiceResponse<Employee>();
+            _mapper = mapper;
+        }
+        public ServiceResponse<EmployeesDTO> AddEmployee(EmployeesDTO employee)
+        {
+            ServiceResponse<EmployeesDTO> serviceResponse = new ServiceResponse<EmployeesDTO>();
             serviceResponse.Data = employee;
-            empList.Add(employee);
+            empList.Add(_mapper.Map<Employee>(employee));
             return serviceResponse;
         }
 
-        public ServiceResponse<Employee> GetEmployee(int id)
+        public ServiceResponse<EmployeesDTO> GetEmployee(int id)
         {
-            ServiceResponse<Employee> serviceResponse = new ServiceResponse<Employee>();
-            serviceResponse.Data = empList.FirstOrDefault(employee => employee.Id == id);
+            ServiceResponse<EmployeesDTO> serviceResponse = new ServiceResponse<EmployeesDTO>();
+            serviceResponse.Data = _mapper.Map<EmployeesDTO>(empList.FirstOrDefault(employee => employee.Id == id));
             return serviceResponse;
         }
 
-        public ServiceResponse<List<Employee>> GetEmployees()
+        public ServiceResponse<List<EmployeesDTO>> GetEmployees()
         {
-            ServiceResponse<List<Employee>> serviceResponse = new ServiceResponse<List<Employee>>();
-            serviceResponse.Data = empList;
+            ServiceResponse<List<EmployeesDTO>> serviceResponse = new ServiceResponse<List<EmployeesDTO>>();
+            serviceResponse.Data = empList.Select(employee => _mapper.Map<EmployeesDTO>(employee)).ToList();
             return serviceResponse; 
         }
     }
