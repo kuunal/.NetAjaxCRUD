e9bab86aebcd96a6041ac58dbf4b81d1da5ddf16
kuunal <kunaldeshmukh2503@gmail.com>
2020-11-25 18:04:53 +0530

    [Kunal] ADD: Registered dependency for Automapper

--- Greeting/Startup.cs
+++ Greeting/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Greeting.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -28,6 +29,7 @@ namespace Greeting
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddScoped<IService, EmployeeServices>();
+            services.AddAutoMapper(typeof(Startup));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
