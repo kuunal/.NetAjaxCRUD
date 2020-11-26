1b51f6b1e3bc5d40224e2c60e43fecf8d7534a2c
kuunal <kunaldeshmukh2503@gmail.com>
2020-11-26 10:52:29 +0530

    [Kunal] ADD: Interface for repository

---
+++ Greeting/Repositories/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using Greeting.DTOs.EmployeeDTO;
+using Greeting.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Greeting.Repositories
+{
+    public interface IRepository<T>
+    {
+        List<T> Get();
+        T Get(int id);
+        void Add(T employeeData);
+
+        void Remove(int id);
+        T Update(int id);
+    }
+}
