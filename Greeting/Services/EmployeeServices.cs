e59c87a251fbf2b72a7d54aee3d20c82ec511d17
kuunal <kunaldeshmukh2503@gmail.com>
2020-11-26 10:51:49 +0530

    [Kunal] ADD: Implementation for abstract methods

--- Greeting/Services/EmployeeServices.cs
+++ Greeting/Services/EmployeeServices.cs
@@ -1,47 +1,57 @@
 ﻿using AutoMapper;
 using Greeting.DTOs.EmployeeDTO;
 using Greeting.Models;
+using Greeting.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace Greeting.Services
 {
     public class EmployeeServices : IService
     {
-        List<Employee> empList = new List<Employee>() {
-            new Employee{ Id=1, Name="f0rest", Email="f0rest@sk.com", address="NY", PhoneNumber=1234567890},
-            new Employee{ Id=2, Name="GEt_Right", Email="Get_Right@sk.com", address="NY", PhoneNumber=1234567890},
-            new Employee{ Id=3, Name="fallEn", Email="fallen@sk.com", address="NY", PhoneNumber=1234567890},
-            new Employee{ Id=4, Name="Spawn", Email="Spawn@sk.com", address="NY", PhoneNumber=1234567890},
-        };
-
         IMapper _mapper;
-        public EmployeeServices(IMapper mapper)
-        {
+        IRepository<Employee> _repo;
+        public EmployeeServices(IMapper mapper, IRepository<Employee> repo)
+        { 
             _mapper = mapper;
+            _repo = repo;
         }
         public ServiceResponse<EmployeesDTO> AddEmployee(EmployeesDTO employee)
         {
             ServiceResponse<EmployeesDTO> serviceResponse = new ServiceResponse<EmployeesDTO>();
             serviceResponse.Data = employee;
-            empList.Add(_mapper.Map<Employee>(employee));
+            _repo.Add(_mapper.Map<Employee>(employee));
             return serviceResponse;
         }
 
         public ServiceResponse<EmployeesDTO> GetEmployee(int id)
         {
             ServiceResponse<EmployeesDTO> serviceResponse = new ServiceResponse<EmployeesDTO>();
-            serviceResponse.Data = _mapper.Map<EmployeesDTO>(empList.FirstOrDefault(employee => employee.Id == id));
+            serviceResponse.Data = _mapper.Map<EmployeesDTO>(_repo.Get(id));
+            return serviceResponse;
+        }
+
+        
+        public ServiceResponse<EmployeesDTO> RemoveEmployee(int id)
+        {
+            ServiceResponse<EmployeesDTO> serviceResponse = new ServiceResponse<EmployeesDTO>();
+            _repo.Remove(id);
+            serviceResponse.Message = "Deleted successfully"; 
             return serviceResponse;
         }
 
         public ServiceResponse<List<EmployeesDTO>> GetEmployees()
         {
             ServiceResponse<List<EmployeesDTO>> serviceResponse = new ServiceResponse<List<EmployeesDTO>>();
-            serviceResponse.Data = empList.Select(employee => _mapper.Map<EmployeesDTO>(employee)).ToList();
+            List<Employee> employees = _repo.Get();
+            serviceResponse.Data = employees.Select(employee => _mapper.Map<EmployeesDTO>(employee)).ToList();
             return serviceResponse; 
+        }
+
+        public ServiceResponse<EmployeesDTO> UpdateEmployee(EmployeesDTO employee)
+        {
+            throw new NotImplementedException();
         }
     }
 }
