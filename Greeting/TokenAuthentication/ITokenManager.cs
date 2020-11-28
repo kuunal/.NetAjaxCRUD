e41426372373accdb7ecb08aa44d73335a40a137
kuunal <kunaldeshmukh2503@gmail.com>
2020-11-28 17:26:57 +0530

    [Kunal] ADD: Abstract methods for JWT encode and decode

---
+++ Greeting/TokenAuthentication/ITokenManager.cs
@@ -0,0 +1,16 @@
+﻿using Greeting.DTOs.EmployeeDTO;
+using Greeting.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Greeting.TokenAuthentication
+{
+    public interface ITokenManager
+    {
+        string Encode(Employee employee);
+        ClaimsPrincipal Decode(string token);
+    }
+}
