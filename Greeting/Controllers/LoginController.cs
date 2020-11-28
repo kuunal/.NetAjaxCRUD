d10e10b83ca8e884dcba032e560a3977cc2f2109
kuunal <kunaldeshmukh2503@gmail.com>
2020-11-28 17:16:46 +0530

    [Kunal] ADD: Login controller

---
+++ Greeting/Controllers/LoginController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmailService;
+using Greeting.DTOs.EmployeeDTO;
+using Greeting.Models;
+using Greeting.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Greeting.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : Controller
+    {
+        private ILoginService<LoginDTO> _service;
+
+        public LoginController(ILoginService<LoginDTO> loginService)
+        {
+            _service = loginService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AuthenticateUser(LoginDTO user)
+        {
+            Employee employee = (await _service.SearchByEmail(user.Email)).Data;
+            if (employee != null)
+                return Ok(await _service.AuthenticateEmployee(user));
+            return BadRequest(new { message = "Email Already exist!" });
+        }
+
+       
+    }
+}
