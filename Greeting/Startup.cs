0d5e962126981b726975b7227815f122769d52e4
kuunal <kunaldeshmukh2503@gmail.com>
2020-11-28 18:53:10 +0530

    [Kunal] ADD: Configuration data for send email

--- Greeting/Startup.cs
+++ Greeting/Startup.cs
@@ -1,12 +1,16 @@
 ﻿using AutoMapper;
+using EmailService;
+using Greeting.DTOs.EmployeeDTO;
 using Greeting.Models;
 using Greeting.Repositories;
 using Greeting.Services;
+using Greeting.TokenAuthentication;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace Greeting
 {
@@ -23,12 +27,18 @@ namespace Greeting
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            var emailConfig = Configuration
+                .GetSection("EmailConfiguration")
+                .Get<EmailConfiguration>();
+            services.AddSingleton(emailConfig);
+            services.AddScoped<IEmailSender, EmailSender>();
             services.AddScoped<IService, EmployeeServices>();
+            services.AddScoped<ILoginRepository<LoginDTO>, LoginRepository>();
+            services.AddScoped<ILoginService<LoginDTO>, LoginService>();
             services.AddScoped<IRepository<Employee>, Repository>();
+            services.AddTransient<ITokenManager, TokenManager>();
             services.AddAutoMapper(typeof(Startup));
-            services.AddCors(options=>options.AddDefaultPolicy(
-                builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowCredentials().AllowAnyMethod()
-                ));
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,8 +53,11 @@ namespace Greeting
                 app.UseHsts();
             }
 
-            app.UseCors();
-            app.UseHttpsRedirection();
+            //app.UseCors();
+            app.UseCors(
+                options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader().AllowCredentials()
+            );
+            //app.UseHttpsRedirection();
             app.UseMvc();
         }
     }
