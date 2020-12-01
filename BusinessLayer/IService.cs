12bdd56a7b5f034f9dae7beb4f2e624f028935c8
kuunal <kunaldeshmukh2503@gmail.com>
2020-12-01 18:18:15 +0530

    [Kunal] REFACTOR: Removed support for generics

---
+++ BusinessLayer/IService.cs
@@ -0,0 +1,18 @@
+﻿
+using ModelLayer;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public interface IService
+    {
+        Task<List<Employee>> GetEmployees();
+        Task<Employee> GetEmployee(int id);
+        Task<Employee> AddEmployee(Employee employee);
+        Task<int> RemoveEmployee(int id);
+        Task<Employee> UpdateEmployee(int id, Employee employee);
+
+
+    }
+}
