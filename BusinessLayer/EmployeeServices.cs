b8c112649b9abb84e76478e192a71ff25916d6db
kuunal <kunaldeshmukh2503@gmail.com>
2020-12-01 18:19:00 +0530

    [Kunal] REFACTOR: Exception handling

---
+++ BusinessLayer/EmployeeServices.cs
@@ -0,0 +1,76 @@
+﻿using AutoMapper;
+using ModelLayer;
+using RepositoryLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class EmployeeServices : IService
+    {
+        IMapper _mapper;
+        IRepository<Employee> _repo;
+        public EmployeeServices(IMapper mapper, IRepository<Employee> repo)
+        { 
+            _mapper = mapper;
+            _repo = repo;
+        }
+        public async Task<Employee> AddEmployee(Employee employee)
+        {
+            try { 
+                return await _repo.Add(employee);
+            }catch(Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        public async Task<Employee> GetEmployee(int id)
+        {
+            try {
+                return await _repo.GetAsync(id);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+
+        public async Task<int> RemoveEmployee(int id)
+        {
+            try { 
+                return await _repo.Remove(id);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        public async Task<List<Employee>> GetEmployees()
+        {
+            try
+            {
+                return await _repo.GetAsync();
+            }catch(Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        public async Task<Employee> UpdateEmployee(int id, Employee data)
+        {
+            try { 
+                return (await _repo.Update(id, data));
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+    }
+}
---
+++ BusinessLayer/LoginService.cs
@@ -0,0 +1,58 @@
+﻿using EmailService;
+using ModelLayer;
+using RepositoryLayer;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using TokenAuthentication;
+
+namespace BusinessLayer
+{
+    public class LoginService : ILoginService<LoginDTO>
+    {
+        ILoginRepository<LoginDTO> _repository;
+        private IEmailSender _emailSender;
+        ITokenManager _tokenManager;
+        public LoginService(ILoginRepository<LoginDTO> loginRepository
+            , ITokenManager tokenManager
+            , IEmailSender emailSender)
+        {
+            _repository = loginRepository;
+            _emailSender = emailSender;
+            _tokenManager = tokenManager;
+        }
+        public async Task<(Employee, string)> AuthenticateEmployee(LoginDTO user)
+        {
+            Employee employee = await _repository.Login(user);
+            if (employee != null)
+            {
+                string token = _tokenManager.Encode(employee);
+                return (employee,token);
+            }
+            return (null, null);
+        }
+
+        public async Task<Employee> SearchByEmail(string email)
+        {
+            return await _repository.GetByEmail(email);
+        }
+
+        public async Task ForgotPassword(Employee employee, string currentUrl)
+        {
+            string jwt = _tokenManager.Encode(employee);
+            string url = "https://"+currentUrl+"/html/reset.html?"+jwt;
+            Message message = new Message(new string[] { employee.Email },
+                    "Password Reset Email",
+                    $"<h6>Click on the link to reset password<h6><a href='{url}'>{jwt}</a>");
+            await _emailSender.SendEmail(message);
+        }
+
+        public async Task<int> ResetPassword(string password, string token)
+        {
+            ClaimsPrincipal claims = _tokenManager.Decode(token);
+            var claim =  claims.Claims.ToList();
+            //claim;
+            return (await _repository.ResetPassword(claim[0].Value, password));
+        }
+    }
+}
