05931723bdc1afc35ec42d94f376cc7063da6c0c
kuunal <kunaldeshmukh2503@gmail.com>
2020-12-02 00:42:52 +0530

    [Kunal] ADD: logging using Nlog

--- Greeting/Program.cs
+++ Greeting/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
 
 namespace Greeting
 {
@@ -19,6 +20,11 @@ namespace Greeting
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+            .ConfigureLogging((hostingContext, logging) =>
+            {
+                logging.ClearProviders();
+                logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                logging.AddNLog();
+             }).UseStartup<Startup>();
     }
 }
