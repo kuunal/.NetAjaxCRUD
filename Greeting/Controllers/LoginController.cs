01feb7e1f6e3ef08ba5444cd44bc5a337fb02852
kuunal <kunaldeshmukh2503@gmail.com>
2020-12-01 18:26:32 +0530

    [Kunal] ADD: Exception ahndling and generating response

--- Greeting/Controllers/LoginController.cs
+++ Greeting/Controllers/LoginController.cs
@@ -2,12 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BusinessLayer;
 using EmailService;
-using Greeting.DTOs.EmployeeDTO;
-using Greeting.Models;
-using Greeting.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ModelLayer;
 
 namespace Greeting.Controllers
 {
@@ -25,7 +24,18 @@ namespace Greeting.Controllers
         [HttpPost]
         public async Task<IActionResult> AuthenticateUser([FromForm] LoginDTO user)
         {
-            return Ok(await _service.AuthenticateEmployee(user));
+            try { 
+                var (userData, token) = await _service.AuthenticateEmployee(user);
+                if (userData == null || token == null)
+                {
+                    return BadRequest(new ServiceResponse<Employee>(null, 400, "Invalid id or password"));
+                }
+                return Ok(new ServiceResponse<Employee>(userData, 200, token));
+            }catch(Exception e)
+            {
+                return BadRequest(new ServiceResponse<Employee>(null, 400, e.Message));
+            }
+
         }
 
         [HttpPost("{email}")]
@@ -33,14 +43,14 @@ namespace Greeting.Controllers
         [ApiExplorerSettings(IgnoreApi = true)]
         public async Task<IActionResult> ForgotPassword([FromForm] string email)
         {
-            Employee employee = (await _service.SearchByEmail(email)).Data;
+            Employee employee = await _service.SearchByEmail(email);
             if (employee != null)
             {
                 var currentUrl = HttpContext.Request.Host;
                 await _service.ForgotPassword(employee, currentUrl.Value);
-                return Ok(new { message = "Mail sent for resetting password" });
+                return Ok(new { message = "Mail sent for resetting password" , status=200});
             }
-            return BadRequest(new { error = "Email doenst exists" });
+            return BadRequest(new { error = "Email doenst exists", status = 400 });
         }
 
         [HttpPost("{password, token}")]
@@ -49,8 +59,8 @@ namespace Greeting.Controllers
         public async Task<IActionResult> Reset([FromForm] string password, [FromForm] string token)
         {
             if (await _service.ResetPassword(password, token) == 1)
-                return Ok();
-            return BadRequest();
+                return Ok(new { status = 200, message = "Password Reset successfull!"});
+            return BadRequest(new { status = 400, message = "Password Reset Failed!" });
         }
     }
 }
