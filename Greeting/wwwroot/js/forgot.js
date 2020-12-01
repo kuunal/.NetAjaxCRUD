43b5d8739c7092b1bcbd004db47ec2cb46578991
kuunal <kunaldeshmukh2503@gmail.com>
2020-12-01 18:22:34 +0530

    [Kunal] ADD: Animated error message and validation of email

--- Greeting/wwwroot/js/forgot.js
+++ Greeting/wwwroot/js/forgot.js
@@ -1,18 +1,34 @@
-$(document).ready(function(){
+function displayError(error) {
+    $('.error').addClass("error-active");
+    setTimeout(function () {
+        $('.error').text(error);
+    }, 250);
+    setTimeout(function () {
+        $('.error').removeClass("error-active").text('');
+    }, 5000);
+}
 
+$(document).ready(function () {
     $(document).on("click","#forgot-btn",function(e){
-    console.log("saddddddddddddddddddddddddddddddddddd")
-    e.preventDefault();
+        e.preventDefault
         let email = $('#email-id').val();
+        if (!email.match("^[a-zA-Z0-9]+[\\.\\-\\+\\_]?[a-zA-Z0-9]+@[a-zA-Z0-9]+[.]?[a-zA-Z]{2,4}[\\.]?([a-z]{2,4})?$")) {
+            displayError("INVALID EMAIL FORMAT");
+            return
+        }
         $.ajax({
             url:"/forgot",
             method:"POST",
             data:{
                 Email : email
             }, success: function(){
-                alert("Please check mail");
-            },error: function(){
-                alert("Wrong emal id")
+                displayError('Mail has been sent');
+            }, error: function (err) {
+                if (err.status == 400) {
+                    displayError('Invalid Id or password');
+                }
+                else if (err.status >= 500)
+                    $('.error').text('Server is down! ');
             }
         });
     })
