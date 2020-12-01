e7ddd96bb9eb7c3ca4afe2846b49813c627456e8
kuunal <kunaldeshmukh2503@gmail.com>
2020-12-01 18:16:52 +0530

    [Kunal] REFACTOR: Return type

---
+++ BusinessLayer/ILoginService.cs
@@ -0,0 +1,15 @@
+﻿using ModelLayer;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public interface ILoginService<T>
+    {   
+        Task<(Employee, string)> AuthenticateEmployee(T user);
+        Task<Employee> SearchByEmail(string email);
+
+        Task ForgotPassword(Employee employee, string currentUrl);
+        Task<int> ResetPassword(string password, string token);
+
+    }
+}
