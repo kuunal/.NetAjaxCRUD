97e60c8109fd51e5834999e01144c21a1b2f68eb
kuunal <kunaldeshmukh2503@gmail.com>
2020-12-01 18:23:43 +0530

    [Kunal] ADD: Model Layer for Gretting app

---
+++ ModelLayer/ServiceResponse.cs
@@ -0,0 +1,16 @@
+﻿namespace ModelLayer
+{
+    public class ServiceResponse<T>
+    {
+        public T Data;
+
+        public int StatusCode;
+        public string Message;
+
+        public ServiceResponse(T data, int status, string message){
+            this.Data = data;
+            this.StatusCode = status;
+            this.Message = message;
+        }
+    }
+}
