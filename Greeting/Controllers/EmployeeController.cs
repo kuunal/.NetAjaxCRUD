3a4d65ac918311b78f921a3d95c65c2cc27baba3
kuunal <kunaldeshmukh2503@gmail.com>
2020-12-01 18:26:04 +0530

    [Kunal] ADD: Exception handling

--- Greeting/Controllers/EmployeeController.cs
+++ Greeting/Controllers/EmployeeController.cs
@@ -2,12 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Greeting.DTOs.EmployeeDTO;
-using Greeting.Models;
-using Greeting.Services;
+using BusinessLayer;
 using Greeting.TokenAuthentication;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
+using ModelLayer;
 
 namespace Greeting.Controllers
 {
@@ -23,37 +22,90 @@ namespace Greeting.Controllers
         }
 
         [HttpGet]
+        [Route("/Employee")]
         [TokenAuthenticationFilter]
         public async Task<IActionResult> GetEmployeeAsync()
         {
-            return Ok(await _empService.GetEmployees());
+            try {
+                List<Employee> employee = await _empService.GetEmployees();
+                if (employee == null)
+                {
+                    return Ok(new ServiceResponse<Employee>(null, 200, "No data!"));
+                }
+                    return Ok(new ServiceResponse<List<Employee>>(employee , 200, "No data!"));
+            }catch(Exception e)
+            {
+                return BadRequest(new ServiceResponse<Employee>(null, 500, e.Message));
+            }
         }
 
         [HttpGet("{id}")]
         [TokenAuthenticationFilter]
         public async Task<IActionResult> DetailEmployeeAsync(int id)
         {
-            return Ok(await _empService.GetEmployee(id));
+            try {
+                Employee employeeData = await _empService.GetEmployee(id);
+                if (employeeData == null)
+                {
+                    return BadRequest(new ServiceResponse<Employee>(employeeData, 400, "NO such user"));
+                }
+                return Ok(new ServiceResponse<Employee>(employeeData, 200, "Successfull"));
+            }catch(Exception e)
+            {
+                return BadRequest(new ServiceResponse<Employee>(null, 500, e.Message));
+            }
         }
 
         [HttpPost]
-        public async Task<IActionResult> AddEmployeeAsync([FromForm] EmployeesDTO employee)
-        {   
-            return Ok(await _empService.AddEmployee(employee));
+        public async Task<IActionResult> AddEmployeeAsync([FromForm] Employee employee)
+        {
+            try {
+                Employee addedEmployee = await _empService.AddEmployee(employee);
+                if (addedEmployee == null)
+                {
+                    return BadRequest(new ServiceResponse<Employee>(null, 400, "Failed to add record"));
+                }
+                return Ok(new ServiceResponse<Employee>(addedEmployee, 200, "Addded Successfully"));
+            }
+            catch(Exception e)
+            {
+                return BadRequest(new ServiceResponse<Employee>(null, 500, e.Message));
+            }
         }
 
         [HttpDelete("{id}")]
         [TokenAuthenticationFilter]
         public async Task<IActionResult> DeleteEmployeeAsync(int id)
         {
-            return Ok(await _empService.RemoveEmployee(id));
+            try { 
+                int result = await _empService.RemoveEmployee(id);
+                if (result == 0)
+                {
+                    return BadRequest(new ServiceResponse<Employee>(null, 400, "Failed to Delete"));
+                }
+                return Ok(new ServiceResponse<Employee>(null, 200, "Deleted Successfully"));
+            }catch(Exception e)
+            {
+                return BadRequest(new ServiceResponse<Employee>(null, 500, e.Message));
+            }
         }
 
         [HttpPut("{id}")]
         [TokenAuthenticationFilter]
-        public async Task<IActionResult> EditEmployeeAsync(int id, [FromForm] EmployeesDTO updatedEmployee)
+        public async Task<IActionResult> EditEmployeeAsync(int id, [FromForm] Employee updatedEmployee)
         {
-            return Ok(await _empService.UpdateEmployee(id, updatedEmployee));
+            try { 
+                Employee employee = await _empService.UpdateEmployee(id, updatedEmployee);
+                if (employee == null)
+                {
+                    return BadRequest(new ServiceResponse<Employee>(null, 400, "Failed to Update"));
+                }
+                    return Ok(new ServiceResponse<Employee>(employee, 200, "Updated employee data"));
+            }
+            catch (Exception e)
+            {
+                return BadRequest(new ServiceResponse<Employee>(null, 500, e.Message));
+            }
         }
 
     }
