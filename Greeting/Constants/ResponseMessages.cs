9e2f0ced76661524fb2c167ff1dff050a5a60602
kuunal <kunaldeshmukh2503@gmail.com>
2020-12-05 09:51:54 +0530

    [Kunal] ADD: File dfor constant resposne messages

---
+++ Greeting/Constants/ResponseMessages.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Greeting.Constants
+{
+    public class ResponseMessages
+    {
+        public const string NO_SUCH_USER = "No such user!";
+        public const string FAILED = "FAILED!";
+        public const string SUCCESSFULL = "Successful!";
+        public const string NO_DATA_AVAILABLE = "No data available!";
+        public const string INVALID_CREDENTIALS = "Invalid Id or password!";
+        public const string MAIL_SENT = "Mail sent for resetting password";
+        public const string PASSWORD_CHANGED = "Password Changed";
+        public const string NO_DATA_PROVIDED = "Empty fields!";
+    }
+}
