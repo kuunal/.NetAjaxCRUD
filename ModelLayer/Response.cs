de708c4dc52fe54dfdd0046fad7120a75974e2d0
kuunal <kunaldeshmukh2503@gmail.com>
2020-12-15 15:19:24 +0530

    [Kunal] REFACTOR: Orderin for SMD format

--- ModelLayer/Response.cs
+++ ModelLayer/Response.cs
@@ -2,15 +2,15 @@
 {
     public class Response<T>
     {
-        public T Data;
 
         public int StatusCode;
         public string Message;
+        public T Data;
 
         public Response(T data, int status, string message){
-            this.Data = data;
             this.StatusCode = status;
             this.Message = message;
+            this.Data = data;
         }
     }
 }
